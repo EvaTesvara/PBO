bae2247330440d47b2963630c0d3f27cafcffde7
EvaTesvara <98539851+EvaTesvara@users.noreply.github.com>
2022-04-02 09:25:57 +0700

    Problem set 6 _ 3
    shape drawing method with loop

---
+++ LoopShapeTest.java
@@ -0,0 +1,13 @@
+package loopshape;
+
+public class LoopShapeTest {
+    public static void main(String[] args) {
+        
+        LoopShape.createRectangle(10,5);
+
+        System.out.println();
+        System.out.println();
+
+        LoopShape.createTriangle(6);
+    }   
+}
